3923f62886b526987109cfc22ee16c873ca7a8bf
Tanausú Falcón <alu0101320878@ull.edu.es>
2022-05-03 16:11:23 +0000

    Prueba con mocha y chai

---
+++ tests/requestClient.spec.ts
@@ -0,0 +1,21 @@
+import 'mocha';
+import {expect} from 'chai';
+import {EventEmitter} from 'events';
+import * as net from 'net';
+import {RequestClient} from '../src/clientNotes';
+
+describe('RequestClient', () => {
+  it('Should emit a response event once it gets a complete message', (done) => {
+    const socket = new net.Socket();
+    const client = new RequestClient(socket);
+
+    client.on('response', (response) => {
+      expect(response).to.be.eql('{"type": "add", "success": true}');
+      done();
+    });
+
+    socket.emit('data', '{"type": "add"');
+    socket.emit('data', ', "success": true}');
+    socket.emit('data', '\n');
+  });
+});
